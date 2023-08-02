17aa28bc016ed4f0f4a9aa9a5bbe63fae3026200
girishgangard <vishwasgangarde09@gmail.com>
2023-08-02 22:35:27 +0530

    first commit

---
+++ allurefile.bat
@@ -0,0 +1,2 @@
+pytest -v -n=4 --alluredir="C:\Users\DELL E5480\Desktop\tushar sir automation\31 july\CredKart_Pytest_Project - Copy\allure" "C:\Users\DELL E5480\Desktop\
+tushar sir automation\31 july\CredKart_Pytest_Project - Copy\testCases\test_Login_params.py"
